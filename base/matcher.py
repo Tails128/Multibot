d151ae3bdf8fbf80f26f4679e197ad6cf5bc0d69
Tails128 <defendifilippo@gmail.com>
2018-05-12 20:59:59 +0200

    added pre and post trigger matchers! Still missing tag matchingand processing.

--- base/matcher.py
+++ base/matcher.py
@@ -5,20 +5,48 @@ class Matcher():
     """Matcher for message items."""
 
     @staticmethod
-    def __fullMatch(candidate, message):
+    def __fullMatch(candidate, message, preMessage, postMessage):
         """Check if the message and the candidate match more deeply."""
         hasPre = "trigger_pre" in candidate
         if hasPre:
-            hasPre = len(candidate.get("trigger_pre").strip(" "))
+            hasPre = len(candidate.get("trigger_pre")) > 0
         hasPost = "trigger_extra" in candidate
         if hasPost:
-            hasPost = len(candidate.get("trigger_extra").strip(" "))
+            hasPost = len(candidate.get("trigger_extra")) > 0
 
-        if not hasPre and not hasPost:
-            return True
+        # If message has no pre-trigger conditions confirm the match for
+        # pre-trigger conditions.
+        if not hasPre:
+            hasPre = True
+        else:
+            tags = Matcher.__getTags(preMessage)
+            if len(tags) > 0:
+                # TODO
+                hasPre = False
+            else:
+                pre_messages = candidate.get("trigger_pre")
+                for pre_message in pre_messages:
+                    print(pre_message + " | " + preMessage)
+                    hasPre = pre_message in preMessage
+                    if(hasPre):
+                        break
 
-        return False
-        # TODO
+        if not hasPost:
+            hasPost = True
+        else:
+            tags = Matcher.__getTags(postMessage)
+            if len(tags) > 0:
+                # TODO
+                hasPost = False
+            else:
+                post_messages = candidate.get("trigger_extra")
+                for post_message in post_messages:
+                    print(post_message + " | " + postMessage)
+                    hasPost = post_message in postMessage
+                    if(hasPost):
+                        break
+
+        return hasPre and hasPost
 
     @staticmethod
     def matches(candidate, message, botname):
@@ -27,7 +55,7 @@ class Matcher():
 
         # if trigger's empty, delegate to fullMatch
         if candidate['trigger'] == '':
-            return Matcher.__fullMatch(candidate, message)
+            return Matcher.__fullMatch(candidate, message, message, message)
 
         # if trigger's a /command, try to match it
         elif candidate.get('trigger')[0] == '/':
@@ -36,17 +64,37 @@ class Matcher():
                 return False
             splittedMessage = message.split(' ')
             if splittedMessage[0] == splittedCandidate[0]:
-                return Matcher.__fullMatch(candidate, message)
+                postMessage = message.lstrip(candidate.get('trigger'))
+                postMessage = postMessage.lstrip(" ")
+                return Matcher.__fullMatch(candidate, message, '', postMessage)
             return False
 
         # if trigger is botname, check if the message contains the botname,
         # then delegate to fullMatch
         elif candidate['trigger'] == 'botname':
             splittedMessage = message.split(' ')
-            for splitted in splittedMessage:
-                if botname == splitted:
-                    return Matcher.__fullMatch(candidate, message)
+            if botname in splittedMessage:
+                newSplit = message.split(botname)
+                if(len(newSplit) is not 2):
+                    return False
+                else:
+                    return Matcher.__fullMatch(candidate, message, newSplit[0],
+                                               newSplit[1])
             return False
 
         # default return false
         return False
+
+    @staticmethod
+    def __getTags(string):
+        answer = []
+        tempAnswer = string.split("}")
+        if len(tempAnswer) is 1:
+            return []
+        for candidate in tempAnswer:
+            print(candidate)
+            candidate = candidate[candidate.index("{") + 1:]
+            # TODO also check validity
+            if candidate is not "user":
+                answer.append(candidate)
+        return answer
--- base/matcher_test.py
+++ base/matcher_test.py
@@ -10,20 +10,66 @@ def test_simple_botname_matches():
     """
     botName = "test"
     candidate = {'trigger': 'botname'}
-    message = botName
-    assert(matcher.Matcher.matches(candidate, message, botName))
+
+    message = botName + " noise"
+    assert matcher.Matcher.matches(candidate, message, botName)
+
+    message = botName + "sss"
+    assert not matcher.Matcher.matches(candidate, message, botName)
 
 
-def test_simple_botname_does_not_match():
-    """Test the matches function on a botname trigger, with the wrong message.
+def test_pre_botname_matches():
+    """Test the matches function on a botname trigger, with the right message.
 
-    The matches function is tested on a botname trigger without pre or post
-    filters, the message sent is expected NOT to match.
+    The matches function is tested on a botname trigger with pre filter and no
+    post filter, the message sent is expected to match.
     """
     botName = "test"
-    candidate = {'trigger': 'botname'}
-    message = botName + "ssss"
-    assert not (matcher.Matcher.matches(candidate, message, botName))
+    tokens = ['hello', 'hi']
+    candidate = {'trigger': 'botname', 'trigger_pre': tokens}
+
+    for token in tokens:
+        message = token + " noise " + botName
+        assert matcher.Matcher.matches(candidate, message, botName)
+
+    assert not matcher.Matcher.matches(candidate, botName, botName)
+
+
+def test_post_botname_matches():
+    """Test the matches function on a botname trigger, with the right message.
+
+    The matches function is tested on a botname trigger with no pre filter and
+    a post filter, the message sent is expected to match.
+    """
+    botName = "test"
+    tokens = ['hello', 'hi']
+    candidate = {'trigger': 'botname', 'trigger_extra': tokens}
+
+    for token in tokens:
+        message = botName + " noise " + token
+        assert matcher.Matcher.matches(candidate, message, botName)
+
+    assert not matcher.Matcher.matches(candidate, botName, botName)
+
+
+def test_full_botname_matches():
+    """Test the matches function on a botname trigger, with the right message.
+
+    The matches function is tested on a botname trigger with no pre filter and
+    a post filter, the message sent is expected to match.
+    """
+    botName = "test"
+    tokens = ['hi', 'hello']
+    candidate = {'trigger': 'botname', 'trigger_extra': tokens,
+                 'trigger_pre': tokens}
+
+    for pre in tokens:
+        for post in tokens:
+            message = pre + " noise " + botName + " noise " + post
+            assert matcher.Matcher.matches(candidate, message, botName)
+
+    message = botName
+    assert not matcher.Matcher.matches(candidate, message, botName)
 
 
 def test_bad_command_ignore():
@@ -35,7 +81,7 @@ def test_bad_command_ignore():
     trigger = "/comm and"
     candidate = {'trigger': trigger}
     message = trigger
-    assert not (matcher.Matcher.matches(candidate, message, botName))
+    assert not matcher.Matcher.matches(candidate, message, botName)
 
 
 def test_simple_command_match():
@@ -47,21 +93,67 @@ def test_simple_command_match():
     botName = "test"
     trigger = "/command"
     candidate = {'trigger': trigger}
+
     message = trigger + " do_stuff right_now"
-    assert (matcher.Matcher.matches(candidate, message, botName))
+    assert matcher.Matcher.matches(candidate, message, botName)
+
+    message = trigger + "sss"
+    assert not matcher.Matcher.matches(candidate, message, botName)
 
 
-def test_simple_command_not_match():
-    """Test the matches function on a /command trigger, with the wrong message.
+def test_pre_command_ignored():
+    """Test the matches function on a /command trigger, with the right message.
 
-    The matches function is tested on a /command trigger, without pre or post
-    filters, the message sent is expected NOT to match.
+    The matches function is tested on a /command trigger, with a pre filter and
+    no post filters, the message sent is expected to be ignored since a command
+    is supposed to be the start of the message and not be contained in a
+    sentence.
     """
     botName = "test"
     trigger = "/command"
-    candidate = {'trigger': trigger}
-    message = trigger + "_this_part_of_the_command"
-    assert not (matcher.Matcher.matches(candidate, message, botName))
+    tokens = ['hi', 'hello']
+    candidate = {'trigger': trigger, 'trigger_pre': tokens}
+    for token in tokens:
+        message = token + " noise " + trigger
+        assert not matcher.Matcher.matches(candidate, message, botName)
+
+
+def test_post_command_matches():
+    """Test the matches function on a /command trigger, with the right message.
+
+    The matches function is tested on a /command trigger, with no pre filters
+    and a post filter, the message sent is expected to match.
+    """
+    botName = "test"
+    trigger = "/command"
+    tokens = ['hi', 'hello']
+    candidate = {'trigger': trigger, 'trigger_extra': tokens}
+    for token in tokens:
+        message = trigger + " noise " + token
+        assert matcher.Matcher.matches(candidate, message, botName)
+
+        message = trigger + token
+        assert not matcher.Matcher.matches(candidate, message, botName)
+
+
+def test_full_command_ignored():
+    """Test the matches function on a botname trigger, with the right message.
+
+    The matches function is tested on a /command trigger, with a pre filter and
+    a post filter, the message sent is expected to be ignored since a command
+    is supposed to be the start of the message and not be contained in a
+    sentence.
+    """
+    botName = "test"
+    trigger = "/command"
+    tokens = ['hi', 'hello']
+    candidate = {'trigger': trigger, 'trigger_extra': tokens,
+                 'trigger_pre': tokens}
+
+    for pre in tokens:
+        for post in tokens:
+            message = pre + " noise " + trigger + " noise " + post
+            assert not matcher.Matcher.matches(candidate, message, botName)
 
 
 def test_simple_empty_match():
@@ -74,6 +166,60 @@ def test_simple_empty_match():
     candidate = {'trigger': ''}
     message = "any input should trigger this."
     assert (matcher.Matcher.matches(candidate, message, botName))
+
+
+def test_pre_empty_matches():
+    """Test the matches function on a '' trigger, with the right message.
+
+    The matches function is tested on a botname trigger with pre filter and no
+    post filter, the message sent is expected to match.
+    """
+    botName = "test"
+    tokens = ['hello', 'hi']
+    candidate = {'trigger': '', 'trigger_pre': tokens}
+
+    for token in tokens:
+        message = token + " noise "
+        assert matcher.Matcher.matches(candidate, message, botName)
+
+    assert not matcher.Matcher.matches(candidate, botName, botName)
+
+
+def test_post_empty_matches():
+    """Test the matches function on a '' trigger, with the right message.
+
+    The matches function is tested on a botname trigger with no pre filters
+    and a post filter, the message sent is expected to match.
+    """
+    botName = "test"
+    tokens = ['hello', 'hi']
+    candidate = {'trigger': '', 'trigger_extra': tokens}
+
+    for token in tokens:
+        message = token + " noise "
+        assert matcher.Matcher.matches(candidate, message, botName)
+
+    assert not matcher.Matcher.matches(candidate, botName, botName)
+
+
+def test_full_empty_matches():
+    """Test the matches function on a '' trigger, with the right message.
+
+    The matches function is tested on a '' trigger with pre filter and
+    post filter, the message sent is expected to match.
+    """
+    botName = "test"
+    tokens = ['hi', 'hello']
+    candidate = {'trigger': '', 'trigger_extra': tokens,
+                 'trigger_pre': tokens}
+
+    for pre in tokens:
+        for post in tokens:
+            message = pre + " noise " + " noise " + post
+            assert matcher.Matcher.matches(candidate, message, botName)
+
+    message = botName
+    assert not matcher.Matcher.matches(candidate, message, botName)
 
 
 def test_bad_trigger_ignored():
